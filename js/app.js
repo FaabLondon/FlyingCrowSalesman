7786f586394c63f2156439f042ac76343cfef8f6
Fabienne Prouteau <faabke@gmail.com>
2018-05-31 21:42:11 +0100

    user can add locations with icons - also added some controls to zoom in and out and rotate map

--- js/app.js
+++ js/app.js
@@ -1,17 +1,14 @@
 window.addEventListener('DOMContentLoaded', () => {
   console.log('JS loaded');
 
+  //************************ VARIABLES ***********************************//
   //geolocate user if possible - By default set in London
   let latitude = 0.1278;
   let longitude = 51.5074;
+  let nbLocations = 0;
+  const maxLocations = 9; //including home as starting point
 
-  //Geolocation does not work...
-  function geoSuccess(position) {
-    latitude = position.coords.latitude;
-    longitude = position.coords.longitude;
-  }
-  navigator.geolocation.watchPosition(geoSuccess);
-
+  //************************ MAP SETUP ************************************//
   //set token on map
   mapboxgl.accessToken = 'pk.eyJ1IjoiZmFhYmtlIiwiYSI6ImNqaHVsMTJvcTBsb2UzcnM3Y3oxbTlmdHQifQ.YPZQk8EmRpMWrtssf733bQ';
 
@@ -23,20 +20,47 @@ window.addEventListener('DOMContentLoaded', () => {
     center: [latitude, longitude]
   });
 
-  //add user on the map - does not work for now despite adding showUserLocation
-  map.addControl(new mapboxgl.GeolocateControl({
-    positionOptions: {
-      enableHighAccuracy: false,
-      timeout: 6000
-    },
-    trackUserLocation: false,
-    showUserLocation: true
-  }));
+  // Add zoom and rotation controls to the map.
+  map.addControl(new mapboxgl.NavigationControl());
 
+  //enables user to click on location to add a marker - 8 locations max + home
   map.on('mousedown', function(e) {
-    let marker = new mapboxgl.Marker()
-      .setLngLat([e.lngLat.lng, e.lngLat.lat])
-      .addTo(map);
+    if(nbLocations < maxLocations){
+      //define marker with DOM element (div)
+      var elt = document.createElement('div');
+      elt.className = 'marker'; //in order to style it
+      //Add house icon if first marker, otherwise pin icon
+      elt.innerHTML = nbLocations === 0 ? '<i class="fas fa-home fa-2x"></i>' : '<i class="fas fa-thumbtack fa-2x"></i>';
+      //add marker to map
+      let marker = new mapboxgl.Marker(elt)
+        .setLngLat([e.lngLat.lng, e.lngLat.lat])
+        .addTo(map);
+      nbLocations++;
+    } else {
+      window.alert('You can add a maximum of 8 destinations for your journey');
+    }
   });
 
 });
+
+
+
+//Geolocation does not work...
+// function geoSuccess(position) {
+//   latitude = position.coords.latitude;
+//   longitude = position.coords.longitude;
+// }
+// navigator.geolocation.watchPosition(geoSuccess);
+
+
+
+//add user on the map - does not work for now despite adding showUserLocation
+//Due to geolocation blocked
+// map.addControl(new mapboxgl.GeolocateControl({
+//   positionOptions: {
+//     enableHighAccuracy: false,
+//     timeout: 6000
+//   },
+//   trackUserLocation: false,
+//   showUserLocation: true
+// }));
