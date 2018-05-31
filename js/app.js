153f43e349d42f8b71f633ce242802202c0b42b8
Fabienne Prouteau <faabke@gmail.com>
2018-05-31 16:53:09 +0100

    user can add markers on the map and get their coordinates - user geolocation not working for now

--- js/app.js
+++ js/app.js
@@ -1,18 +1,19 @@
 window.addEventListener('DOMContentLoaded', () => {
   console.log('JS loaded');
 
-  //set token on map
-  mapboxgl.accessToken = 'pk.eyJ1IjoiZmFhYmtlIiwiYSI6ImNqaHVsMTJvcTBsb2UzcnM3Y3oxbTlmdHQifQ.YPZQk8EmRpMWrtssf733bQ';
-
   //geolocate user if possible - By default set in London
   let latitude = 0.1278;
   let longitude = 51.5074;
 
+  //Geolocation does not work...
   function geoSuccess(position) {
     latitude = position.coords.latitude;
     longitude = position.coords.longitude;
   }
   navigator.geolocation.watchPosition(geoSuccess);
+
+  //set token on map
+  mapboxgl.accessToken = 'pk.eyJ1IjoiZmFhYmtlIiwiYSI6ImNqaHVsMTJvcTBsb2UzcnM3Y3oxbTlmdHQifQ.YPZQk8EmRpMWrtssf733bQ';
 
   //define map object
   const map = new mapboxgl.Map({
@@ -22,6 +23,20 @@ window.addEventListener('DOMContentLoaded', () => {
     center: [latitude, longitude]
   });
 
+  //add user on the map - does not work for now despite adding showUserLocation
+  map.addControl(new mapboxgl.GeolocateControl({
+    positionOptions: {
+      enableHighAccuracy: false,
+      timeout: 6000
+    },
+    trackUserLocation: false,
+    showUserLocation: true
+  }));
 
+  map.on('mousedown', function(e) {
+    let marker = new mapboxgl.Marker()
+      .setLngLat([e.lngLat.lng, e.lngLat.lat])
+      .addTo(map);
+  });
 
 });
