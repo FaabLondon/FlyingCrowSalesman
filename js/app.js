a036f3e283b10dbbcca2f44b8bdec3d6b741dcf1
Fabienne Prouteau <faabke@gmail.com>
2018-06-01 10:47:44 +0100

    added function to calc distance between 2 points in km

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ window.addEventListener('DOMContentLoaded', () => {
   //geolocate user if possible - By default set in London
   let latitude = 0.1278;
   let longitude = 51.5074;
-  let nbLocations = 0;
+  let locationsArr = [];
   const maxLocations = 9; //including home as starting point
 
   //************************ MAP SETUP ************************************//
@@ -20,26 +20,52 @@ window.addEventListener('DOMContentLoaded', () => {
     center: [latitude, longitude]
   });
 
+  //disable double click on map as double click should add icons
+  //single click is used to move the map
+  map.doubleClickZoom.disable();
+
   // Add zoom and rotation controls to the map.
   map.addControl(new mapboxgl.NavigationControl());
 
   //enables user to click on location to add a marker - 8 locations max + home
-  map.on('mousedown', function(e) {
-    if(nbLocations < maxLocations){
+  map.on('dblclick', function(e) {
+    if(locationsArr.length < maxLocations){
       //define marker with DOM element (div)
       var elt = document.createElement('div');
       elt.className = 'marker'; //in order to style it
       //Add house icon if first marker, otherwise pin icon
-      elt.innerHTML = nbLocations === 0 ? '<i class="fas fa-home fa-2x"></i>' : '<i class="fas fa-thumbtack fa-2x"></i>';
+      elt.innerHTML = locationsArr.length === 0 ? '<i class="fas fa-home fa-2x"></i>' : '<i class="fas fa-thumbtack fa-2x"></i>';
       //add marker to map
       let marker = new mapboxgl.Marker(elt)
         .setLngLat([e.lngLat.lng, e.lngLat.lat])
         .addTo(map);
-      nbLocations++;
+      //add the new location to the array of locations
+      locationsArr.push(e.lngLat);
     } else {
       window.alert('You can add a maximum of 8 destinations for your journey');
     }
   });
+
+  document.querySelector('.calcIt').onclick = function() {
+    calculateItinerary();
+  };
+
+  // function to calculate distance between 2 points
+  function calculateDist(pointA, pointB){
+    let from = turf.point([pointA.lat, pointA.lng]);
+    let to = turf.point([pointB.lat, pointB.lng]);
+    let options = {units: 'kilometers'};
+    return distance = turf.distance(from, to, options);
+  }
+
+  
+  function calculateItinerary(){
+    //calculate optimal itinerary
+    console.log(calculateDist(locationsArr[0], locationsArr[1]));
+
+
+  }
+
 
 });
 
