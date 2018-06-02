d7ec1a511b99ac8bd25c093f1199c5a64d52d9a5
Fabienne Prouteau <faabke@gmail.com>
2018-06-03 00:20:42 +0100

    added map class and methods, added map reset, bug with layer-route when trying to add more points - error still happening despite error catching

--- js/app.js
+++ js/app.js
@@ -4,18 +4,135 @@
 window.addEventListener('DOMContentLoaded', () => {
   console.log('JS loaded');
 
-  //************************ VARIABLES ***********************************//
+  //************************ VARIABLES *****************************************
   //geolocate user if possible - By default set in London
   const latitude = -0.181719;
   const longitude = 51.519739;
-  const locationsArr = [];
+  let locationsArr = [];
   const maxLocations = 9; //including home as starting point
   let startEndPoint = null;
   let bestRouteArr = [];
   let shortestDist = null;
 
-  //*********** Add event listener to button to calculate optimal route *******
-  document.querySelector('.calcIt').onclick = function() {
+  //************************************** Class Map ***************************
+  class Map {
+
+    constructor(){
+      const markers = [];
+
+      //set token on map
+      mapboxgl.accessToken = 'pk.eyJ1IjoiZmFhYmtlIiwiYSI6ImNqaHVsMTJvcTBsb2UzcnM3Y3oxbTlmdHQifQ.YPZQk8EmRpMWrtssf733bQ';
+
+      //define map object
+      const map = new mapboxgl.Map({
+        container: 'map', //id of container
+        style: 'mapbox://styles/faabke/cjhul1zge0vof2sk24i14174u',
+        zoom: 13, // starting zoom
+        center: [latitude, longitude]
+      });
+
+      //disable double click on map as double click should add icons
+      //single click is used to move the map
+      map.doubleClickZoom.disable();
+
+      // Add zoom and rotation controls to the map.
+      map.addControl(new mapboxgl.NavigationControl());
+
+      //******************* Add location event *********************************
+
+      //enables user to click on location to add a marker - 8 locations max + home
+      map.on('dblclick', function(e) {
+        if(locationsArr.length < maxLocations){
+          //define marker with DOM element (div)
+          var elt = document.createElement('div');
+          elt.className = 'marker'; //in order to style it
+          //Add house icon if first marker, otherwise pin icon
+          elt.innerHTML = locationsArr.length === 0 ? '<i class="fas fa-home fa-2x"></i>' : '<i class="fas fa-thumbtack fa-2x"></i>';
+          //add marker to map
+          markers.push(new mapboxgl.Marker(elt)
+            .setLngLat([e.lngLat.lng, e.lngLat.lat])
+            .addTo(map));
+          //add the new location to the array of locations. It includes the starting point (home)
+          locationsArr.push([e.lngLat.lng, e.lngLat.lat]);
+        } else {
+          window.alert('You can add a maximum of 8 destinations for your journey');
+        }
+      });
+
+      this.markers = markers;
+      this.map = map;
+    }
+
+    // *********************** drawBestRoute ***********************************
+    drawBestRoute(){
+      //remove previous layer in case user keeps adding new points
+      //still gets error message despite catch err...
+      try {
+        this.map.removeLayer('route');
+        this.map.removeSource('route');
+      } catch (err){
+        // nothing
+      }
+      // try {map.removeSource('route')} catch (err);
+      this.map.addLayer({
+        "id": "route",
+        "type": "line",
+        "source": {
+            "type": "geojson",
+            "data": {
+                "type": "Feature",
+                "properties": {},
+                "geometry": {
+                    "type": "LineString",
+                    "coordinates": bestRouteArr
+                }
+            }
+        },
+        "layout": {
+            "line-join": "round",
+            "line-cap": "round"
+        },
+        "paint": {
+            "line-color": "#ff0066",
+            "line-width": 4
+        }
+      });
+    }
+
+    // *********************** Reset *******************************************
+    destroy() {
+      //remove the markers
+      this.markers.forEach(marker => marker.remove());
+      //re-initialise global variables - not great practice...
+      locationsArr = [];
+      startEndPoint = null;
+      bestRouteArr = [];
+      shortestDist = null;
+      document.querySelector('.distance').innerHTML = '';
+      //this.map = null;  //do not delete instance of map as can not draw on it anymore so keep the same instance all the time
+      //remove the route
+      try {
+        this.map.removeLayer('route');
+        this.map.removeSource('route');
+      } catch (err){
+        // nothing
+      }
+    }
+
+  }
+
+  //*********************** create map instance ********************************
+
+  const map = new Map();
+
+  //****** Add event listener to button to calculate optimal route and reset ***
+
+  document.querySelector('.calcIt').onclick = calculate;
+  document.querySelector('.reset').onclick = map.destroy.bind(map);
+
+
+  // *********************** calculate Itinerary *******************************
+  function calculate() {
     switch(locationsArr.length){
       case 0:
         window.alert('You need at least 2 locations in your itinerary');
@@ -23,60 +140,12 @@ window.addEventListener('DOMContentLoaded', () => {
       case 1:
         window.alert('You need at least another location in your itinerary');
         break;
-      case 2: //if only 2 points in itinerary
-        shortestDist = calculateTotalDist(locationsArr);
-        bestRouteArr = locationsArr.slice();
-        break;
       default:
         calculateItinerary(locationsArr);
     }
-    console.log('shortestDist',shortestDist);
-    console.log('bestRouteArr',bestRouteArr);
+  }
 
-  };
-
-  //************************ MAP SETUP ************************************//
-  //set token on map
-  mapboxgl.accessToken = 'pk.eyJ1IjoiZmFhYmtlIiwiYSI6ImNqaHVsMTJvcTBsb2UzcnM3Y3oxbTlmdHQifQ.YPZQk8EmRpMWrtssf733bQ';
-
-  //define map object
-  const map = new mapboxgl.Map({
-    container: 'map', //id of container
-    style: 'mapbox://styles/faabke/cjhul1zge0vof2sk24i14174u',
-    zoom: 13, // starting zoom
-    center: [latitude, longitude]
-  });
-
-  //disable double click on map as double click should add icons
-  //single click is used to move the map
-  map.doubleClickZoom.disable();
-
-  // Add zoom and rotation controls to the map.
-  map.addControl(new mapboxgl.NavigationControl());
-
-  //******************* Add location event ************************************
-
-  //enables user to click on location to add a marker - 8 locations max + home
-  map.on('dblclick', function(e) {
-    if(locationsArr.length < maxLocations){
-      //define marker with DOM element (div)
-      var elt = document.createElement('div');
-      elt.className = 'marker'; //in order to style it
-      //Add house icon if first marker, otherwise pin icon
-      elt.innerHTML = locationsArr.length === 0 ? '<i class="fas fa-home fa-2x"></i>' : '<i class="fas fa-thumbtack fa-2x"></i>';
-      //add marker to map
-      new mapboxgl.Marker(elt)
-        .setLngLat([e.lngLat.lng, e.lngLat.lat])
-        .addTo(map);
-      //add the new location to the array of locations. It includes the starting point (home)
-      locationsArr.push([e.lngLat.lng, e.lngLat.lat]);
-    } else {
-      window.alert('You can add a maximum of 8 destinations for your journey');
-    }
-  });
-
-
-  // function to calculate distance between 2 points
+  //************function to calculate distance between 2 points*****************
   function calculateDist(pointA, pointB){
     const from = turf.point(pointA);
     const to = turf.point(pointB);
@@ -85,7 +154,7 @@ window.addEventListener('DOMContentLoaded', () => {
   }
 
 
-  //function to calculate the total distance of an itinerary
+  //*********function to calculate the total distance of an itinerary***********
   function calculateTotalDist(itineraryArr){
     //initialise with distance from last point back home
     let totalDist = calculateDist(itineraryArr[itineraryArr.length - 1], itineraryArr[0]);
@@ -97,7 +166,7 @@ window.addEventListener('DOMContentLoaded', () => {
   }
 
 
-  //function that returns an array with all permutations
+  //**************function that returns an array with all permutations**********
   //(not including starting point and endpoint)
   function CalcPermutations(itinerary){
     const permutations = [];
@@ -113,30 +182,31 @@ window.addEventListener('DOMContentLoaded', () => {
       //recursive call on the points left in the itinerary
       const innerPermutations = CalcPermutations(pointsLeft);
       for (let j = 0; j < innerPermutations.length; j++) {
-        //innerPermutations can be an array with one or several arrays of coordinates [[ {lng, lat}, {lng, lat} ], ...,  ...] hence the test below.
+        //innerPermutations can be an array with one or several arrays of coordinates [ [[lng, lat], [lng, lat]] ], ...,  ...] hence the test below.
         innerPermutations.length > 1 ?
           permutations.push([firstPoint, ...innerPermutations[j]]) : permutations.push([firstPoint, innerPermutations[j]]);
       }
     }
-    console.log('permutation', permutations);
     return permutations;
   }
 
-  // function to calculate optimal itinerary
+  //**************function to calculate optimal itinerary***********************
   function calculateItinerary(itinerary){
 
-    //remove starting point from itinerary
-    startEndPoint = itinerary.splice(0,1)[0];
+    //get starting point from itinerary
+    startEndPoint = itinerary[0];
 
     //intialise all itineraries, best route array and shortest distance
-    const itinerariesArr = CalcPermutations(itinerary); //returns an array of arrays of all itineraries without start and end point
+    const itinerariesArr = CalcPermutations(itinerary.slice(1)); //returns an array of arrays of all itineraries without start and end point
+    console.log('Nb of permutations !(n - 1)', itinerariesArr.length);
     //Define initial best route as one of the permutation with the start and end point
-    bestRouteArr = [startEndPoint, ...itinerariesArr[0], startEndPoint];
+    bestRouteArr = itinerariesArr.length > 1 ? [startEndPoint, ...itinerariesArr[0], startEndPoint] : [startEndPoint, itinerariesArr[0], startEndPoint];
+
     shortestDist = calculateTotalDist(bestRouteArr);
 
     //if the itinerary has a total distance smaller than the current shortest distance
     itinerariesArr.forEach(itin => {
-      const currentRoute = [startEndPoint, ...itin, startEndPoint];
+      const currentRoute = itin[0].length > 1 ? [startEndPoint, ...itin, startEndPoint] : [startEndPoint, itin, startEndPoint];
       const currentTotalDist = calculateTotalDist(currentRoute);
       if (currentTotalDist < shortestDist){
         bestRouteArr = currentRoute.slice();
@@ -144,35 +214,9 @@ window.addEventListener('DOMContentLoaded', () => {
       }
     });
 
-    //once best itinerary is found draw it on the map
-    drawBestRoute();
-  }
-
-  function drawBestRoute(){
-    
-    map.addLayer({
-      "id": "route",
-      "type": "line",
-      "source": {
-          "type": "geojson",
-          "data": {
-              "type": "Feature",
-              "properties": {},
-              "geometry": {
-                  "type": "LineString",
-                  "coordinates": bestRouteArr
-              }
-          }
-      },
-      "layout": {
-          "line-join": "round",
-          "line-cap": "round"
-      },
-      "paint": {
-          "line-color": "#ff0066",
-          "line-width": 4
-      }
-    });
+    //once best itinerary is found draw it on the map and update the distance displayed
+    map.drawBestRoute();
+    document.querySelector('.distance').innerHTML = `The shortest route will be <span> ${Math.round(shortestDist*100)/100} km </span> long`;
   }
 
 });
