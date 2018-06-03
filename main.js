9700a6276d487dcf5383ef5555e2e9265a610de9
Fabienne Prouteau <faabke@gmail.com>
2018-06-03 13:53:14 +0100

    Changed main.js to try to deploy

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
+const express = require('express');
+const PORT = process.env.PORT || 8000;
+const app = express();
+
+app.use(express.static(`${__dirname}/public`));
+app.listen(PORT, () => console.log(`Server up on port ${PORT}`));
+
+
 module.exports = {
   collect: require('@turf/collect'),
   buffer: require('@turf/buffer')
